5a000dd1cb78353fbb0a08ac90158fbb91948c29
33gael <33gael.dev@gmail.com>
2025-04-24 13:14:15 +0200

    Valid C07

--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ultimate_range.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qludger <qludger@student.42.fr>            +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 14:44:33 by qludger           #+#    #+#             */
-/*   Updated: 2025/04/19 18:32:37 by qludger          ###   ########.fr       */
+/*   Updated: 2025/04/24 11:53:31 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,22 @@
 int ft_ultimate_range(int **range, int min, int max)
 {
     int i;
+    int size;
 
     if(min >= max)
     {
         *range = NULL;
         return(0);
     }
-    *range = malloc(sizeof(int) * (max - min));
+    size = max - min;
+    *range = malloc(sizeof(int) * size);
     if (*range == 0)
         return (0);
     i = 0;
-    while (min < max)
+    while (i < size)
     {
-        (*range)[i] = min;
+        (*range)[i] = min + i;
         i++;
-        min++;
     }
     return (i);
 }
